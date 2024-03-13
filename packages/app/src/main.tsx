eeb9817b4e1e5d0e283a14b28dc955de651884b0
Pier Francesco Ferrari <pierfrancesco@commercelayer.io>
2024-03-13 13:06:32 +0100

    feat: setup the app with list page

---
+++ packages/app/src/main.tsx
@@ -0,0 +1,43 @@
+import {
+  CoreSdkProvider,
+  ErrorBoundary,
+  MetaTags,
+  PageSkeleton,
+  TokenProvider,
+  createApp
+} from '@commercelayer/app-elements'
+import '@commercelayer/app-elements/style.css'
+import { StrictMode } from 'react'
+import { SWRConfig } from 'swr'
+import { App } from './App'
+
+const isDev = Boolean(import.meta.env.DEV)
+
+createApp(
+  (props) => (
+    <StrictMode>
+      <ErrorBoundary hasContainer>
+        <SWRConfig
+          value={{
+            revalidateOnFocus: false
+          }}
+        >
+          <TokenProvider
+            kind='price_lists'
+            appSlug='price_lists'
+            devMode={isDev}
+            reauthenticateOnInvalidAuth={!isDev && props?.onInvalidAuth == null}
+            loadingElement={<PageSkeleton />}
+            {...props}
+          >
+            <CoreSdkProvider>
+              <MetaTags />
+              <App routerBase={props?.routerBase} />
+            </CoreSdkProvider>
+          </TokenProvider>
+        </SWRConfig>
+      </ErrorBoundary>
+    </StrictMode>
+  ),
+  'price_lists'
+)
---
+++ packages/app/src/pages/PriceListsList.tsx
@@ -0,0 +1,88 @@
+import { ListItemPriceList } from '#components/ListItemPriceList'
+import { instructions } from '#data/filters'
+import {
+  Button,
+  EmptyState,
+  HomePageLayout,
+  useResourceFilters,
+  useTokenProvider
+} from '@commercelayer/app-elements'
+import { Link } from 'wouter'
+import { navigate, useSearch } from 'wouter/use-browser-location'
+
+export function PriceListsList(): JSX.Element {
+  const { canUser } = useTokenProvider()
+
+  const queryString = useSearch()
+
+  const { SearchWithNav, FilteredList, hasActiveFilter } = useResourceFilters({
+    instructions
+  })
+
+  if (!canUser('read', 'price_lists')) {
+    return (
+      <HomePageLayout title='Price Lists'>
+        <EmptyState title='You are not authorized' />
+      </HomePageLayout>
+    )
+  }
+
+  return (
+    <HomePageLayout title='Price Lists'>
+      <SearchWithNav
+        queryString={queryString}
+        onUpdate={(qs) => {
+          navigate(`?${qs}`, {
+            replace: true
+          })
+        }}
+        onFilterClick={() => {}}
+        hideFiltersNav
+      />
+      <FilteredList
+        type='price_lists'
+        query={{
+          sort: {
+            created_at: 'desc'
+          }
+        }}
+        ItemTemplate={ListItemPriceList}
+        emptyState={
+          hasActiveFilter ? (
+            <EmptyState
+              title='No Price lists found!'
+              description={
+                <div>
+                  <p>We didn't find any Price list matching the search.</p>
+                </div>
+              }
+              action={
+                canUser('create', 'price_lists') && (
+                  <Link href='#'>
+                    <Button variant='primary'>Add a Price list</Button>
+                  </Link>
+                )
+              }
+            />
+          ) : (
+            <EmptyState
+              title='No Price lists yet!'
+              action={
+                canUser('create', 'price_lists') && (
+                  <Link href='#'>
+                    <Button variant='primary'>Add a Price list</Button>
+                  </Link>
+                )
+              }
+            />
+          )
+        }
+        actionButton={
+          canUser('create', 'price_lists') ? (
+            <Link href='#'>Add new</Link>
+          ) : undefined
+        }
+      />
+    </HomePageLayout>
+  )
+}
