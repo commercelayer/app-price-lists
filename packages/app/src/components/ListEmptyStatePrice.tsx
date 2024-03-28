7d6815ea84e1e7d5954f2d6782498ea686be9e09
Pier Francesco Ferrari <pierfrancesco@commercelayer.io>
2024-03-28 17:13:26 +0100

    fix: add missing new price button action in empty state

--- packages/app/src/components/ListEmptyStatePrice.tsx
+++ packages/app/src/components/ListEmptyStatePrice.tsx
@@ -1,3 +1,4 @@
+import { appRoutes } from '#data/routes'
 import {
   A,
   Button,
@@ -5,16 +6,32 @@ import {
   useTokenProvider
 } from '@commercelayer/app-elements'
 import type { FC } from 'react'
+import { useLocation, useRoute } from 'wouter'
 
 export const ListEmptyStatePrice: FC = () => {
   const { canUser } = useTokenProvider()
+  const [, setLocation] = useLocation()
+
+  const [, params] = useRoute<{ priceListId: string }>(
+    appRoutes.pricesList.path
+  )
+  const priceListId = params?.priceListId ?? ''
 
   if (canUser('create', 'prices')) {
     return (
       <EmptyState
         title='No Prices yet!'
         description='Create your first Price'
-        action={<Button variant='primary'>New Price</Button>}
+        action={
+          <Button
+            variant='primary'
+            onClick={() => {
+              setLocation(appRoutes.priceNew.makePath({ priceListId }))
+            }}
+          >
+            New Price
+          </Button>
+        }
       />
     )
   }
